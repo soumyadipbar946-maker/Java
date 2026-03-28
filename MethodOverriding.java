d240f3381b3cb011f3bc21204bdde98b2e4eb091
Soumyadip Bar <soumyadipbar946@gmail.com>
2026-03-28 19:13:57 +0530

    Create MethodOverriding.java

---
+++ MethodOverriding.java
@@ -0,0 +1,17 @@
+class Animal {
+    void sound(){
+        System.out.println("Animal makes a sound");
+    }
+}
+class Dog extends Animal {
+    void sound(){
+        System.out.println("Dog barks");
+    } 
+}
+
+public class MethodOverriding{
+    public static void main(String[] args) {
+        Dog d = new Dog();
+        d.sound();
+    }
+}
